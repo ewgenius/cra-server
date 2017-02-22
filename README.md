5d41e7dd3dd7cf7deb6f4e15eda48cdb0c2b1397
Evgeniy Khramkov <ewgeniux@gmail.com>
2017-02-22 11:46:21 +0300

    README

---
+++ README.md
@@ -0,0 +1,20 @@
+# CRA Server
+
+simple express static server with opt-in proxy
+
+## How to install
+
+```
+npm install --dave-dev cra-server
+```
+
+add in yout package.json:
+
+```
+...
+"scripts": {
+  ...
+  "serve": "cra-server"
+  ...
+}
+```
--- src/cli.ts
+++ src/cli.ts
@@ -7,3 +7,10 @@ const PROXY_PREFIX = process.env.PROXY_PREFIX || '';
 const PROXY_URL = process.env.PROXY_URL || 'https://localhost:5000';
 const STATIC_DIR = process.env.STATIC_DIR || './build';
 
+createServer({
+  port: PORT,
+  proxyPrefix: PROXY_PREFIX,
+  proxyUrl: PROXY_URL,
+  staticDir: STATIC_DIR
+});
+
